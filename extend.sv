962a6a67bb14854ec178fd5fbc29eda1aed14953
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-22 12:45:58 -0800

    Update extend.sv

--- extend.sv
+++ extend.sv
@@ -1,22 +1,19 @@
-module regfile(input  logic        clk, 
-               input  logic        we3, 
-               input  logic [ 4:0] a1, a2, a3, 
-               input  logic [31:0] wd3, 
-               output logic [31:0] rd1, rd2);
+`timescale 1ns / 1ps
 
-  logic [31:0] rf[31:0];
-
-  // three ported register file
-  // read two ports combinationally (A1/RD1, A2/RD2)
-  // write third port on rising edge of clock (A3/WD3/WE3)
-  // register 0 hardwired to 0
-
-  always_ff @(posedge clk)
-    if(we3)
-      rf[a3]<=wd3;
-  
-  assign rd1 = (a1!=0)?rf[a1]:0;
-  assign rd2 = (a2!=0)?rf[a2]:0;
-
-  
+module extend(input  logic [31:7] InstrD,
+              input  logic [1:0]  ImmSrcD,
+              output logic [31:0] ImmExtD);
+ 
+  always_comb
+    case(ImmSrcD) 
+               
+      2'b00: ImmExtD = {{20{InstrD[31]}}, InstrD[31:20]};  // I-type 
+               // 
+      2'b01: ImmExtD =  {{20{InstrD[31]}}, InstrD[31:25], InstrD[11:7]};   // S-type (stores)
+               // 
+      2'b10: ImmExtD = {{20{InstrD[31]}}, InstrD[7], InstrD[30:25], InstrD[11:8], 1'b0};  // B-type (branches)
+               // 
+      2'b11: ImmExtD = {{12{InstrD[31]}}, InstrD[19:12], InstrD[20], InstrD[30:21], 1'b0};   // J-type (jal)
+      default: ImmExtD = 32'bx; // undefined
+    endcase             
 endmodule
