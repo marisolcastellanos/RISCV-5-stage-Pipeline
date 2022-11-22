47d8a58571fa36e7989a56ec31d7a9d677d03d7a
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-22 13:39:09 -0800

    Create id_ex.sv

---
+++ id_ex.sv
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+
+module id_ex(
+
+    input   logic       clk,reset,
+    input   logic [1:0] ResultSrcD,
+    input   logic       MemWriteD,
+    input   logic       ALUSrcD,
+    input   logic       RegWriteD, JumpD,BranchD,
+    input   logic [2:0] ALUControlD,
+    
+
+                        
+    
+
+    );
+endmodule
