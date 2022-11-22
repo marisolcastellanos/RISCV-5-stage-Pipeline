f3f200d446ba4817bc21733ce1d9cc09d56bf820
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-22 14:49:09 -0800

    Update id_ex.sv

--- id_ex.sv
+++ id_ex.sv
@@ -5,26 +5,27 @@
 module id_ex(
 
     input   logic           clk,reset,
-    input   logic [1:0]     ResultSrcD,
     input   logic           MemWriteD,
     input   logic           ALUSrcD,
     input   logic           RegWriteD, JumpD,BranchD,
+    input   logic [1:0]     ResultSrcD,
     input   logic [2:0]     ALUControlD,
     input   logic [5:0]     RdD
     input   logic [31:0]    RD1D,RD2D, 
                             PCD,
                             ImmExtD, 
                             PCPlus4D, 
-    output  logic [1:0]     ResultSrcE,
     output  logic           MemWriteE,
     output  logic           ALUSrcE,
     output  logic           RegWriteE, JumpE,BranchE,
-    output  logic [2:0]     ALUControlE,
-    output  logic [5:0]     RdE
-    output  logic [31:0]    RD1D,RD2D, 
-                            PCD,
-                            ImmExtD, 
-                            PCPlus4D
+    output  logic [1:0]     ResultSrcE,
+    output  logic [2:0]     ALUControlE)
+    output  logic [4:0]     RdE,
+    output  logic [31:0]    PCE,
+                            RD1E,
+                            RD2E,
+                            ImmExtE, 
+                            PCPlus4E);
     
     
     
