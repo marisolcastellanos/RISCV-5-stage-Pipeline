46e60ad7b0a12cea78202d6d842936b60578ddaa
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-22 17:13:16 -0800

    Update id_ex.sv

--- id_ex.sv
+++ id_ex.sv
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 
-
-
 module id_ex(
 
     input   logic           clk,reset,
@@ -27,12 +25,36 @@ module id_ex(
                             ImmExtE, 
                             PCPlus4E);
     
-    
-    
-    
-
-                        
-    
-
-    );
+    always_ff@(posedge clk,posedge reset)
+    if (reset) begin 
+        MemWriteE <= 0; 
+        ALUSrcE <= 0; 
+        RegWriteE <= 0; 
+        JumpE <= 0; 
+        BranchE <= 0; 
+        ResultSrcE <= 0; 
+        ALUControlE <= 0; 
+        RdE <= 0; 
+        PCE <= 0; 
+        RD1E <= 0; 
+        RD2E <= 0; 
+        ImmExtE <= 0; 
+        PCPlus4E <= 0; 
+    end 
+    else begin 
+        MemWriteE <= MemWriteD; 
+        ALUSrcE <= ALUSrcD; 
+        RegWriteE <= RegWriteD; 
+        JumpE <= JumpD; 
+        BranchE <= BranchD; 
+        ResultSrcE <= ResultSrcD; 
+        ALUControlE <= ALUControlD; 
+        RdE <= RdD; 
+        PCE <= PCD; 
+        RD1E <= RD1D; 
+        RD2E <= RD2D; 
+        ImmExtE <= ImmExtD; 
+        PCPlus4E <= PCPlus4D; 
+    end              
+ 
 endmodule
