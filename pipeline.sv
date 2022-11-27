8c1579a19f6050f6f345a4d5f9138e20e12250f9
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-27 00:57:29 -0800

    Update pipeline.sv

--- pipeline.sv
+++ pipeline.sv
@@ -48,4 +48,13 @@ logic   reset;
    
    idecode idecode1(clk,reset,tb_RegWriteD, tb_InstrD, tb_PCD, tb_PCPlus4D, tb_ResultW, tb_MemWriteE, tb_ALUSrcE,tb_RegWriteE, tb_JumpE, tb_BranchE, tb_ResultSrcE, tb_ALUControlE,tb_RdE,tb_PCE,tb_RD1E,tb_RD2E,tb_ImmExtE,tb_PCPlus4E);
    
+  
+//~~~~~~~~~~~~~~~Execute Module~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+   logic [31:0]    tb_ALUResultM, tb_WriteDataM, tb_PCPlus4M;
+   logic [4:0]     tb_RdM;
+   logic           tb_MemWriteM, tb_RegWriteM;
+   logic [1:0]     tb_ResultSrcM;
+   
+   execute execute1(clk,reset,tb_RD1E,tb_RD2E,tb_ImmExtE, tb_PCE,tb_PCPlus4E,tb_RdE,tb_RegWriteE, tb_MemWriteE, tb_JumpE,tb_BranchE,tb_ALUSrcE,tb_ResultSrcE,tb_ALUControlE,tb_ALUResultM,tb_WriteDataM,tb_PCPlus4M,tb_PCTargetE,tb_RdM,tb_MemWriteM,tb_RegWriteM,tb_ResultSrcM); 
+     
 endmodule // pipeline
