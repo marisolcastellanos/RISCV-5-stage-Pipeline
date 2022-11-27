c4245fb66642efb5501243aec150c419dda44385
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-27 03:56:16 -0800

    Update controller.sv

--- controller.sv
+++ controller.sv
@@ -3,18 +3,22 @@
 module controller(input  logic [6:0] opD,
                   input  logic [2:0] funct3D,
                   input  logic       funct7b5D,
-                  output logic       MemWriteD,
-                  output logic       ALUSrcD,
-                  output logic       RegWriteD, JumpD,BranchD,
+                  output logic       RegWriteD,
                   output logic [1:0] ResultSrcD,
-                  output logic [1:0] ImmSrcD,
-                  output logic [2:0] ALUControlD);
+                  output logic       MemWriteD, JumpD,BranchD,
+                  output logic [2:0] ALUControlD,
+                  output logic       ALUSrcD,
+                  output logic [1:0] ImmSrcD);
 
   logic [1:0] ALUOp;
+  logic       PCSrcE;
+  logic       JumpE;
+  logic       BranchE; 
+  logic       ZeroE;
   //logic       Branch;
 
-  maindec md(opD, ResultSrcD, MemWriteD, BranchD,
-             ALUSrcD, RegWriteD, JumpD, ImmSrcD, ALUOp);
+  maindec md(opD, ResultSrcD, RegWriteD, BranchD,
+             ALUSrcD, MemWriteD, JumpD, ImmSrcD, ALUOp);
   aludec  ad(opD[5], funct3D, funct7b5D, ALUOp, ALUControlD);
 
   //assign PCSrcE = BranchE & ZeroE | JumpE;
