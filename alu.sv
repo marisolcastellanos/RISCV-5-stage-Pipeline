ddde585a3176392d7832c5c2941a02d0ade74f26
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-27 00:54:27 -0800

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -2,30 +2,30 @@
 
 
 module alu(input  logic [31:0] a, b,
-           input  logic [2:0]  alucontrol,
-           output logic [31:0] result,
+           input  logic [2:0]  ALUControlE,
+           output logic [31:0] ALUResultE,
            output logic        ZeroE);
 
   logic [31:0] condinvb, sum;
   logic        v;              // overflow
   logic        isAddSub;       // true when is add or subtract operation
 
-  assign condinvb = alucontrol[0] ? ~b : b;
-  assign sum = a + condinvb + alucontrol[0];
-  assign isAddSub = ~alucontrol[2] & ~alucontrol[1] |
-                    ~alucontrol[1] & alucontrol[0];
+  assign condinvb = ALUControlE[0] ? ~b : b;
+  assign sum = a + condinvb + ALUControlE[0];
+  assign isAddSub = ~ALUControlE[2] & ~ALUControlE[1] |
+                    ~ALUControlE[1] & ALUControlE[0];
 
   always_comb
-    case (alucontrol)
-      3'b000: result=sum; 
-      3'b001: result=sum; 
-      3'b101: result=sum[31]^v;
-      3'b011: result=a|b;
-      3'b010: result=a&b;
-      default: result=32'bx;
+    case (ALUControlE)
+      3'b000: ALUResultE=sum; 
+      3'b001: ALUResultE=sum; 
+      3'b101: ALUResultE=sum[31]^v;
+      3'b011: ALUResultE=a|b;
+      3'b010: ALUResultE=a&b;
+      default: ALUResultE=32'bx;
     endcase
 
-  assign ZeroE = (result == 0) ? 1 : 0;
-  assign v = ~(alucontrol[0] ^ a[31] ^ b[31]) & (a[31] ^ sum[31]) & isAddSub;
+  assign ZeroE = (ALUResultE == 0) ? 1 : 0;
+  assign v = ~(ALUControlE[0] ^ a[31] ^ b[31]) & (a[31] ^ sum[31]) & isAddSub;
   
 endmodule
