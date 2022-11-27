373634feb1d4f7d7d40711bf5fc0ce341e95ad1f
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-26 23:58:47 -0800

    Update execute.sv

--- execute.sv
+++ execute.sv
@@ -6,7 +6,7 @@ module execute(
     input  logic         clk,reset,
     input  logic [31:0]  RD1E,RD2E,ImmExtE,PCE,PCPlus4E,
     input  logic [4:0]   RdE,
-    input  logic         RegWriteE,MemWriteE,JumpE,BranchE,AUSrcE,
+    input  logic         RegWriteE,MemWriteE,JumpE,BranchE,ALUSrcE,
     input  logic [1:0]   ResultSrcE, 
     input  logic [2:0]   ALUControlE, 
     output logic [31:0]  ALUResutM,WriteDataM,PCPlus4M, PCTargetE,
@@ -16,10 +16,15 @@ module execute(
     
     logic   PCSrcE;
     logic   ZeroE; 
+    logic   SrcBE;
+    logic   ALUResultE;
     
     
     assign PCSrcE = BranchE & ZeroE | JumpE;
     
+    mux2 #(32) srcbmux(RD2E,ImmExtE,ALUSrcE,SrcBE);
+    adder      PCaddBRANCH(PCE,ImmExtE,PCTargetE);
+    alu        alu(RD1E,SrcBE, ALUControlE,ALUResultE,ZeroE);
     
     
 endmodule
