dcbb97ab955d92321848e69eb2b61a181b5316f1
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-12-03 10:29:21 -0800

    Update aludec.sv

--- aludec.sv
+++ aludec.sv
@@ -2,41 +2,41 @@
 
 
 module aludec(input  logic       opb5,
-              input  logic [2:0] funct3,
-              input  logic       funct7b5, 
+              input  logic [2:0] funct3D,
+              input  logic       funct7b5D, 
               input  logic [1:0] ALUOp,
-              output logic [2:0] ALUControl);
+              output logic [2:0] ALUControlD);
 
   logic  RtypeSub;
-  assign RtypeSub = funct7b5 & opb5;  // TRUE for R-type subtract instruction
+  assign RtypeSub = funct7b5D & opb5;  // TRUE for R-type subtract instruction
 
   always_comb
     case(ALUOp)
      // add the logic here 
-      2'b00: ALUControl = 3'b000; //add
+      2'b00: ALUControlD = 3'b000; //add
         
-      2'b01: ALUControl = 3'b001; //subtract
+      2'b01: ALUControlD = 3'b001; //subtract
           
-      2'b10: case(funct3)
+      2'b10: case(funct3D)
         
-        3'b010: ALUControl = 3'b101; //SLT      
+        3'b010: ALUControlD = 3'b101; //SLT      
             
-        3'b110: ALUControl = 3'b011; //OR 
+        3'b110: ALUControlD = 3'b011; //OR 
             
-        3'b111: ALUControl = 3'b010; //AND
+        3'b111: ALUControlD = 3'b010; //AND
        
-        3'b000: case({opb5,funct7b5}) 
-          2'b11: ALUControl = 3'b001;
+        3'b000: case({opb5,funct7b5D}) 
+          2'b11: ALUControlD = 3'b001;
           
-          2'b00: ALUControl = 3'b000;
+          2'b00: ALUControlD = 3'b000;
                               
-          2'b01: ALUControl = 3'b000;
+          2'b01: ALUControlD = 3'b000;
                               
-          2'b10: ALUControl = 3'b000;
+          2'b10: ALUControlD = 3'b000;
                               
           endcase
            
-        default: ALUControl= 3'bxxx;  
+        default: ALUControlD= 3'bxxx;  
         endcase
     endcase
 endmodule
